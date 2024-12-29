cba55c4e070b2ac99a04b393afc2e2675a68449a
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2024-12-29 05:56:50 +0100

    fix: address

--- src/socket.ts
+++ src/socket.ts
@@ -7,7 +7,7 @@ import { useEffect, useRef } from "react";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://socket-io-3i32.onrender.com';
 
-export const socket = io(URL);
+export const socket = io(URL, { 'multiplex': false, addTrailingSlash: false });
 
 export const socketInit = () => {
 
