2b2f02586792aea2283fb19ec48e4ae225187fae
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2024-12-29 06:52:50 +0100

    feat: allowed origins

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   assetPrefix: process.env.ASSET_PREFIX || "",
   output: 'export',
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['https://socket-io-3i32.onrender.com'],
+    },
+  },
 };
 module.exports = nextConfig;
