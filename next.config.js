ba0174bf1d799e62d643163b3353176ff1300ce5
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2024-12-29 06:33:42 +0100

    feat: rewrites

--- next.config.js
+++ next.config.js
@@ -5,5 +5,13 @@ const nextConfig = {
   assetPrefix: process.env.ASSET_PREFIX || "",
   output: 'export',
   images: { unoptimized: true },
+  async rewrites() {
+    return [
+      {
+        source: '/socket.io',
+        destination: 'https://socket-io-3i32.onrender.com/socket.io',
+      },
+    ]
+  },
 };
 module.exports = nextConfig;
