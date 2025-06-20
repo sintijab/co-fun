16191c4505256e398d6cd2cb9edbf8910527c100
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2025-06-20 15:03:46 +0200

    feat: imports

--- src/utils/socket/socketJobDiscovery.ts
+++ src/utils/socket/socketJobDiscovery.ts
@@ -1,11 +1,11 @@
 "use client";
 
-// import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 
-// // "undefined" means the URL will be computed from the `window.location` object
-// const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://socket-io-career-bot.onrender.com';
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://socket-io-career-bot.onrender.com';
 
-// const socket = io(URL, { 'multiplex': false });
-// // @ts-ignore-next-line
-// socket.io.uri = 'https://socket-io-career-bot.onrender.com';
-// export { socket };
+const socket = io(URL, { 'multiplex': false });
+// @ts-ignore-next-line
+socket.io.uri = 'https://socket-io-career-bot.onrender.com';
+export { socket };
