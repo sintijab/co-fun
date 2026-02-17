815aa8399cefd8fd6b49e71e9a342012c8812414
Sintija Birgele <sintijabirgele@Sintijas-MacBook-Pro.local>
2026-02-17 23:00:14 +0100

    fix: visual templating

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 'use client';
 
-import { Box, CoFunUiProvider, Flex, Footer, NavigationTop, TextBase } from '@co-fun/ui';
+import {
+  Box,
+  CoFunUiProvider,
+  Flex,
+  Footer,
+  NavigationTop,
+  TextBase,
+} from '@co-fun/ui';
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
 import { GlobalStyles } from '../providers/GlobalStyles';
 import { store, persistor } from '../store';
 import { useRouter } from 'next/router';
@@ -21,7 +28,17 @@ export default function App({
   const iframeWidgetDesktop = useRef(null);
   const isChat = router.asPath.includes('chat');
   const isIndex = router.asPath === '/';
-  const isFullLayout = router.asPath.includes('/explore') || router.asPath.includes('/academy') || router.asPath.includes('/trends') || isIndex || isChat || router.asPath === '/home' || router.asPath.includes('/home#') || router.asPath.includes('/career-discovery') || router.asPath.includes('/alchemy');
+  const isFullLayout =
+    router.asPath.includes('/explore') ||
+    router.asPath.includes('/academy') ||
+    router.asPath.includes('/trends') ||
+    isIndex ||
+    isChat ||
+    router.asPath === '/home' ||
+    router.asPath.includes('/home#') ||
+    router.asPath.includes('/career-discovery') ||
+    router.asPath.includes('/alchemy') ||
+    router.asPath.includes('/photography');
 
   // useEffect(() => {
   //   const healthCheck = async () => {
@@ -44,162 +61,266 @@ export default function App({
         <CoFunUiProvider>
           <GlobalStyles />
           <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
           </Head>
-          {router.asPath !== '/explore' && <NavigationTop links={[
-            {
-              label: 'Creators',
-              href: 'https://cofun.digital/explore'
-            },
-            {
-              label: 'Startups',
-              href: 'https://cofun.digital/funding'
-            },
-            {
-              label: 'Features',
-              href: 'https://cofun.digital/features'
-            },
-            {
-              label: 'Products',
-              href: 'https://cofun.digital/products'
-            },
-            {
-              label: 'Pricing',
-              href: 'https://cofun.digital/home#pricing'
-            },
-            {
-              label: 'Contact',
-              href: 'https://cofun.digital/contact',
-            }
-          ]} />}
-          {!isFullLayout && <Flex direction="column" margin={["3rem .5rem", "5rem 10rem", "5rem 20rem"]} flexWrap="wrap">
-            <Component {...pageProps} />
-          </Flex>}
-          {isFullLayout &&
+          {router.asPath !== '/explore' && (
+            <NavigationTop
+              links={[
+                {
+                  label: 'Creators',
+                  href: 'https://cofun.digital/explore',
+                },
+                {
+                  label: 'Startups',
+                  href: 'https://cofun.digital/funding',
+                },
+                {
+                  label: 'Features',
+                  href: 'https://cofun.digital/features',
+                },
+                {
+                  label: 'Products',
+                  href: 'https://cofun.digital/products',
+                },
+                {
+                  label: 'Pricing',
+                  href: 'https://cofun.digital/home#pricing',
+                },
+                {
+                  label: 'Contact',
+                  href: 'https://cofun.digital/contact',
+                },
+              ]}
+            />
+          )}
+          {!isFullLayout && (
+            <Flex
+              direction="column"
+              margin={['3rem .5rem', '5rem 10rem', '5rem 20rem']}
+              flexWrap="wrap"
+            >
+              <Component {...pageProps} />
+            </Flex>
+          )}
+          {isFullLayout && (
             <>
               <Component {...pageProps} />
             </>
-          }
-          {!isChat &&
+          )}
+          {!isChat && (
             <Box m="3.5rem 1rem 1rem" position="relative" height="max-content">
-              <Footer {
-                ...{
+              <Footer
+                {...{
                   columns: [
                     {
                       title: 'Use cases',
                       links: [
                         {
                           label: 'AI assistance',
-                          href: '/assistance'
+                          href: '/assistance',
                         },
                         {
                           label: 'Content optimization',
-                          href: '/optimization'
+                          href: '/optimization',
                         },
                         {
                           label: 'Content generation',
-                          href: '/generation'
+                          href: '/generation',
                         },
                         {
                           label: 'Initiatives',
-                          href: '/initiatives'
+                          href: '/initiatives',
                         },
                         {
                           label: 'Action analysis',
-                          href: '/analysis'
+                          href: '/analysis',
                         },
                         {
                           label: 'Projects',
-                          href: '/projects'
+                          href: '/projects',
                         },
                         {
                           label: 'Team collaboration',
-                          href: '/collaboration'
-                        }
-                      ]
+                          href: '/collaboration',
+                        },
+                      ],
                     },
                     {
                       title: 'Explore',
                       links: [
                         {
                           label: 'Academy',
-                          href: '/academy'
+                          href: '/academy',
                         },
                         {
                           label: 'Integrations',
-                          href: '/integrations'
+                          href: '/integrations',
                         },
                         {
                           label: 'Funding',
-                          href: '/funding'
+                          href: '/funding',
                         },
                         {
                           label: 'Development features',
-                          href: '/development'
+                          href: '/development',
                         },
                         {
                           label: 'Form creation',
-                          href: '/forms'
+                          href: '/forms',
                         },
                         {
                           label: 'Collaboration features',
-                          href: '/collaboration'
+                          href: '/collaboration',
                         },
                         {
                           label: 'Design process',
-                          href: '/features'
-                        }
-                      ]
+                          href: '/features',
+                        },
+                      ],
                     },
                     {
                       title: 'Resources',
                       links: [
                         {
                           label: 'Analytics',
-                          href: '/analytics'
+                          href: '/analytics',
                         },
                         {
                           label: 'Community',
-                          href: '/community'
+                          href: '/community',
                         },
                         {
                           label: 'Events',
-                          href: '/events'
+                          href: '/events',
                         },
                         {
                           label: 'Blog',
-                          href: '/academy'
+                          href: '/academy',
                         },
                         {
                           label: 'Support',
-                          href: '/support'
+                          href: '/support',
                         },
                         {
                           label: 'Developers',
-                          href: '/developers'
+                          href: '/developers',
                         },
                         {
                           label: 'Reource library',
-                          href: '/resources'
-                        }
-                      ]
-                    }
+                          href: '/resources',
+                        },
+                      ],
+                    },
                   ],
-                }
-              } />
-            </Box>}
-            {!isChat && <TextBase textAlign="right" pr=".5rem">© 2024 Sintija Birgele</TextBase>}
-          {isIndex && isChatVisible && <AspectRatio ratio={3 / 4} position="fixed" bottom="4rem" right={["1rem", "4rem"]} zIndex={1} display={["none", "unset"]}>
-            <iframe ref={iframeWidgetDesktop} src="./chat-general" style={{ position: 'unset', borderRadius: "1.1rem", boxShadow: '0 -3px 12px rgba(0,0,0,0.05)', width: '20rem', height: "30rem", padding: '0 .2rem .8rem .2rem', background: 'primary.white' }} />
-          </AspectRatio>}
-          {isIndex && isChatVisible && <AspectRatio ratio={3 / 4} position="fixed" bottom="4rem" right={["1rem", "4rem"]} zIndex={1} display={["unset", "none"]}>
-            <iframe src="./chat-general" style={{ position: 'unset', borderRadius: "1.1rem", boxShadow: '0 -3px 12px rgba(0,0,0,0.05)', width: '20rem', height: "30rem", padding: '0 .2rem .8rem .2rem', background: 'primary.white' }} />
-          </AspectRatio>}
-          {isIndex && <Box background='primary.white' onClick={() => toggleWidget(isChatVisible === null ? true : !isChatVisible)} cursor="pointer" position="fixed" bottom="1rem" right={["1rem", "2rem"]} zIndex={10} boxShadow='0 -3px 12px rgba(0,0,0,0.1)' borderRadius="1.7rem">
-            <Image src="./assistant_logo.png" maxWidth="45px" width="45px" height="45px" />
-            {isChatVisible === null && <Box position="absolute" bottom="-1px" right="0px" width=".75rem" height=".75rem" borderRadius="50%" background="#bd2e95" color="white" fontSize="10pt" display={["none", "unset"]} />}
-            {isChatVisible && <Box position="absolute" bottom="-1px" right="0px" width=".75rem" height=".75rem" borderRadius="50%" background="#bd2e95" color="white" fontSize="10pt" display={["unset", "none"]} />}
-          </Box>}
+                }}
+              />
+            </Box>
+          )}
+          {!isChat && (
+            <TextBase textAlign="right" pr=".5rem">
+              © 2024 Sintija Birgele
+            </TextBase>
+          )}
+          {isIndex && isChatVisible && (
+            <AspectRatio
+              ratio={3 / 4}
+              position="fixed"
+              bottom="4rem"
+              right={['1rem', '4rem']}
+              zIndex={1}
+              display={['none', 'unset']}
+            >
+              <iframe
+                ref={iframeWidgetDesktop}
+                src="./chat-general"
+                style={{
+                  position: 'unset',
+                  borderRadius: '1.1rem',
+                  boxShadow: '0 -3px 12px rgba(0,0,0,0.05)',
+                  width: '20rem',
+                  height: '30rem',
+                  padding: '0 .2rem .8rem .2rem',
+                  background: 'primary.white',
+                }}
+              />
+            </AspectRatio>
+          )}
+          {isIndex && isChatVisible && (
+            <AspectRatio
+              ratio={3 / 4}
+              position="fixed"
+              bottom="4rem"
+              right={['1rem', '4rem']}
+              zIndex={1}
+              display={['unset', 'none']}
+            >
+              <iframe
+                src="./chat-general"
+                style={{
+                  position: 'unset',
+                  borderRadius: '1.1rem',
+                  boxShadow: '0 -3px 12px rgba(0,0,0,0.05)',
+                  width: '20rem',
+                  height: '30rem',
+                  padding: '0 .2rem .8rem .2rem',
+                  background: 'primary.white',
+                }}
+              />
+            </AspectRatio>
+          )}
+          {isIndex && (
+            <Box
+              background="primary.white"
+              onClick={() =>
+                toggleWidget(isChatVisible === null ? true : !isChatVisible)
+              }
+              cursor="pointer"
+              position="fixed"
+              bottom="1rem"
+              right={['1rem', '2rem']}
+              zIndex={10}
+              boxShadow="0 -3px 12px rgba(0,0,0,0.1)"
+              borderRadius="1.7rem"
+            >
+              <Image
+                src="./assistant_logo.png"
+                maxWidth="45px"
+                width="45px"
+                height="45px"
+              />
+              {isChatVisible === null && (
+                <Box
+                  position="absolute"
+                  bottom="-1px"
+                  right="0px"
+                  width=".75rem"
+                  height=".75rem"
+                  borderRadius="50%"
+                  background="#bd2e95"
+                  color="white"
+                  fontSize="10pt"
+                  display={['none', 'unset']}
+                />
+              )}
+              {isChatVisible && (
+                <Box
+                  position="absolute"
+                  bottom="-1px"
+                  right="0px"
+                  width=".75rem"
+                  height=".75rem"
+                  borderRadius="50%"
+                  background="#bd2e95"
+                  color="white"
+                  fontSize="10pt"
+                  display={['unset', 'none']}
+                />
+              )}
+            </Box>
+          )}
         </CoFunUiProvider>
       </PersistGate>
     </Provider>
